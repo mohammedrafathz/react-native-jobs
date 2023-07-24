7b54502cb977a02836336f4628e5ea449165defd
mohammedrafathz <mohammedrafathz@gmail.com>
2023-07-24 15:19:55 -0400

    initial commit

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,16 @@ module.exports = function (api) {
     plugins: [
       "@babel/plugin-proposal-export-namespace-from",
       "react-native-reanimated/plugin",
+      ["module:react-native-dotenv", {
+        "envName": "APP_ENV",
+        "moduleName": "@env",
+        "path": ".env",
+        "blocklist": null,
+        "allowlist": null,
+        "safe": false,
+        "allowUndefined": true,
+        "verbose": false
+      }],
       require.resolve("expo-router/babel"),
     ],
   };
